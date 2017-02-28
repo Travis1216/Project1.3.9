dfcb7d5f2b8b83941c9b759775d61af24a532ae9
Travis1216 <1069943@lammersvilleusd.net>
2017-02-28 14:20:37 -0800

    Fixed the team name

--- prisoners_dilemma.pyStrat.py
+++ prisoners_dilemma.pyStrat.py
@@ -175,12 +175,12 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 3:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'TLMAA'
         else:
             if len(opponent_history)==0: 
-                return 'c'
+                return 'b'
             else:
-                return 'b' 
+                return 'C' 
 
 
 
@@ -196,7 +196,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 4:
         if getting_team_name:
-            return 'You Will Never Know'
+            return 'TLMAA'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
@@ -225,7 +225,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'You Will Never Know'
+            return 'TLMAA'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
@@ -255,16 +255,12 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 6:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'TLMAA'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0:
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'b'
     
 
 
